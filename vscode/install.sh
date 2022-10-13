8608cf8964cfccfcfbe7123cfb71b6fa9172a68c
mcproger <mcproger7@gmail.com>
2022-10-13 12:05:10 +0200

    Add vscode configs

---
+++ vscode/install.sh
@@ -0,0 +1,5 @@
+code --install-extension eamodio.gitlens
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension vscodevim.vim
